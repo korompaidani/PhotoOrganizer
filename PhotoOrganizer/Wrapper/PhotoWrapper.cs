df9d730e9b2a86f91477ede99898f9d9fce00e1d
Dániel Korompai <korompaidani@gmail.com>
2021-01-30 22:30:17 +0100

    refactor validation errors

--- PhotoOrganizer/Wrapper/PhotoWrapper.cs
+++ PhotoOrganizer/Wrapper/PhotoWrapper.cs
@@ -1,20 +1,10 @@
 ﻿using PhotoOrganizer.Model;
-using PhotoOrganizer.ViewModel;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhotoOrganizer.Wrapper
 {
-    public class PhotoWrapper : ViewModelBase, INotifyDataErrorInfo
+    public class PhotoWrapper : NotifyDataErrorInfoBase
     {
-        private Dictionary<string, List<string>> _errorsByPropertyName
-            = new Dictionary<string, List<string>>();
-
         public Photo Model { get; }
         public int Id { get { return Model.Id; } }
 
@@ -79,29 +69,13 @@ namespace PhotoOrganizer.Wrapper
             }
         }
 
-        public bool HasErrors => _errorsByPropertyName.Any();
-
-        public PhotoWrapper(Photo model)
-        {
-            Model = model;
-        }
-
-        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
-
-        public IEnumerable GetErrors(string propertyName)
-        {
-            return _errorsByPropertyName.ContainsKey(propertyName)
-                ? _errorsByPropertyName[propertyName]
-                : null;
-        }
-
         private void ValidateProperty(string propertyName)
         {
             ClearErrors(propertyName);
             switch (propertyName)
             {
                 case nameof(Title):
-                    if(String.Equals(Title, String.Empty, StringComparison.OrdinalIgnoreCase))
+                    if (String.Equals(Title, String.Empty, StringComparison.OrdinalIgnoreCase))
                     {
                         AddError(propertyName, "Title cannot be empty");
                     }
@@ -110,31 +84,9 @@ namespace PhotoOrganizer.Wrapper
 
         }
 
-        private void OnErrorsChanged(string propertyName)
+        public PhotoWrapper(Photo model)
         {
-            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
-        }
-
-        private void AddError(string propertyName, string error)
-        {
-            if (!_errorsByPropertyName.ContainsKey(propertyName))
-            {
-                _errorsByPropertyName[propertyName] = new List<string>();
-            }
-            if (!_errorsByPropertyName[propertyName].Contains(error))
-            {
-                _errorsByPropertyName[propertyName].Add(error);
-                OnErrorsChanged(propertyName);
-            }
-        }
-
-        private void ClearErrors(string propertyName)
-        {
-            if (!_errorsByPropertyName.ContainsKey(propertyName))
-            {
-                _errorsByPropertyName.Remove(propertyName);
-                OnErrorsChanged(propertyName);
-            }
+            Model = model;
         }
     }
 }
