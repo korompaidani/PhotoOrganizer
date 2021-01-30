873527fbe26f506a40b084f97ed973586ffa8e76
Dániel Korompai <korompaidani@gmail.com>
2021-01-30 13:18:55 +0100

    Display texts and image on UI (xaml changes)

--- PhotoOrganizer/App.xaml.cs
+++ PhotoOrganizer/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PhotoOrganizer.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,12 @@ namespace PhotoOrganizer
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = new MainWindow(
+                new MainViewModel(
+                    new PhotoDataService()));
+            mainWindow.Show();
+        }
     }
 }
