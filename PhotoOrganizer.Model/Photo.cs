942eb733948dd8d321acec55425f7667ad957cc5
Dániel Korompai <korompaidani@gmail.com>
2021-01-30 11:07:42 +0100

    Create projects

---
+++ PhotoOrganizer.Model/Photo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoOrganizer.Model
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FullPath { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public TimeSpan HHMMSS { get; set; }
+    }
+}
