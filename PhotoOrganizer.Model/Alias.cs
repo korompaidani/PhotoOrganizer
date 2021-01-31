bf4c45f86518b50ffc3784c7a893ae9fdbe563c5
Dániel Korompai <korompaidani@gmail.com>
2021-01-31 18:30:58 +0100

    People and Alias tables are created

---
+++ PhotoOrganizer.Model/Alias.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhotoOrganizer.Model
+{
+    public class Alias
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nick { get; set; }
+
+        public int PeopleId { get; set; }
+
+        public People People { get; set; }
+    }
+}
---
+++ PhotoOrganizer.Model/People.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhotoOrganizer.Model
+{
+    public class People
+    {
+        public People()
+        {
+            Aliases = new Collection<Alias>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public ICollection<Alias> Aliases { get; set; }
+    }
+}
