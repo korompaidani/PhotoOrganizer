059e2ffc80209eb6b6cac818a7e7e6804afc2790
Dániel Korompai <korompaidani@gmail.com>
2021-03-25 18:27:05 +0100

    Fix load page size timing problem

--- PhotoOrganizer/MainWindow.xaml.cs
+++ PhotoOrganizer/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using MahApps.Metro.Controls;
+﻿using Autofac;
+using MahApps.Metro.Controls;
 using PhotoOrganizer.UI.Engine;
+using PhotoOrganizer.UI.Services;
+using PhotoOrganizer.UI.Startup;
 using PhotoOrganizer.UI.ViewModel;
 using System;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media.Animation;
 
@@ -26,9 +30,18 @@ namespace PhotoOrganizer.UI
 
             ChromiumBrowserEngine.Instance.Initialize();
 
+            await LoadSettingsAsync();
+
             await _viewModel.LoadWorkbenchAsync();
             _viewModel.OpenWorkbenchCommand.Execute(null);
             splashScreen.Visibility = Visibility.Hidden;
+        }
+
+        private async Task LoadSettingsAsync()
+        {
+            var settingsHandler = Bootstrapper.Container.Resolve<ISettingsHandler>();
+            var settings = await settingsHandler.LoadSettingsAsync();
+            await settingsHandler.ApplySettingsAsync(settings);
         }
 
         private void FadeOutSplashScreen()
